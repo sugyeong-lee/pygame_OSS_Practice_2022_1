f5bed6a699b86fee42c8fcaa9b68718dd07e8bec
sugyeong-lee <101383749+sugyeong-lee@users.noreply.github.com>
2022-05-17 03:22:19 +0900

    Update 1_Aoss.py

--- 1_Aoss.py
+++ 1_Aoss.py
@@ -1,115 +1,137 @@
 import pygame
-import os
+import random
+###############################################
+# 기본 초기화
+pygame.init()  # 초기화 (반드시 필요)
 
-
-# 충돌처리 생각하기
-# 1. 로컬 저장소 오류 해결
-# 2. 클래스, 함수 공부하기
-# 3. 같은 이미지 리스트 화면 랜덤 출력
-##################################
-pygame.init()
-# conect test
-
-screen_width = 1200
-screen_height = 800
+# 화면 크기 설정
+screen_width = 1200  # 가로크기
+screen_height = 800  # 세로크기
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-pygame.display.set_caption("KKLHY GAME")
+# 화면 타이틀 설정
+pygame.display.set_caption("Open Source")  # 게임 이름
 
 # FPS
 clock = pygame.time.Clock()
-##################################
+##############################################
 
-# 사용자 게임 초기화
-current_path = os.path.dirname(__file__)
-image_path = os.path.join(current_path, "images")
 
-# 배경 만들기
-background = pygame.image.load(os.path.join(image_path, "background1.png"))
 
-# 캐릭터 만들기
-character = pygame.image.load(os.path.join(image_path, "character1.png"))
-character_size = character.get_rect().size  # 이미지의 크기를 구해옴
-character_width = character_size[0]         # 캐릭터의 가로 크기
-character_height = character_size[1]        # 캐릭터의 세로 크기
-character_x_pos = 0        # 화면 가로의 절반 크기에 해당하는 곳에 위치 (가로위치)
-# 화면 세로 크기 가장 아래에 해당하는 곳에 위치 (세로위치)
-character_y_pos = -80 + screen_height / 2
 
-# 캐릭터 이동 방향 & 속도
-character_to_x_LEFT = 0
-character_to_x_RIGHT = 0
-character_to_y_UP = 0
-character_to_y_DOWN = 0
-character_speed = 1
+# 배경
+background = pygame.image.load("image/background.png")
 
-# 적 enemy 캐릭터
-item = pygame.image.load(os.path.join(image_path, "item.png"))
-item_size = item.get_rect().size  # 이미지의 크기를 구해옴
-item_width = item_size[0]         # 캐릭터의 가로 크기
-item_height = item_size[1]        # 캐릭터의 세로 크기
-item_x_pos = 1200 - item_width / 2         # 화면 가로의 절반 크기에 해당하는 곳에 위치 (가로위치)
-item_y_pos = (screen_height / 2) - (item_height / 2)
+# 캐릭터
+character = pygame.image.load("image/character.png")
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = character_width
+character_y_pos = (screen_height / 2) - (character_height / 2)
 
-item_to_x = 0
-item_to_y = 0
+teacher = pygame.image.load("teacher.png")
+teacher_size = teacher.get_rect().size
+teacher_width = teacher_size[0]
+teacher_height = teacher_size[1]
+teacher_x_pos = screen_width - teacher_width
+teacher_y_pos = (screen_height / 2) - (teacher_height / 2)
 
-# 시작 시간
-start_ticks = pygame.time.get_ticks()     # 현재 tick 을 받아옴
+ball = pygame.image.load("image/ball.png")
+ball_size = ball.get_rect().size
+ball_width = ball_size[0]
+ball_height = ball_size[1]
+ball_x_pos = screen_width - teacher_width
+ball_y_pos = screen_height/2 - ball_height/2
 
-running = True
+# 캐릭터 이동 방향
+character_to_x = 0
+character_to_y = 0
+
+# 캐릭터 이동 속도
+character_speed = 5
+
+# teacher 움직이기
+to_y_teacher = 0
+random_teacher = 600
+
+ball_time = 0
+balls = []
+random_time = random.randrange(10, 100)
+
+
+running = True  # 게임이 진행중인가?
 while running:
-    dt = clock.tick(60)
+    dt = clock.tick(60)  # 게임화면의 초당 프레임 수를 설정
 
+    # 2. 이벤트 처리 (키보드, 마우스)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:          # 캐릭터 왼쪽으로
-                character_to_x_LEFT -= character_speed
-            elif event.key == pygame.K_RIGHT:           # 캐릭터 오른쪽으로
-                character_to_x_RIGHT += character_speed
-            elif event.key == pygame.K_UP:        # 캐릭터 위쪽으로
-                character_to_y_UP -= character_speed
-            elif event.key == pygame.K_DOWN:        # 캐릭터 아래로
-                character_to_y_DOWN += character_speed
-
-        if pygame.time.get_ticks() > 2000:
-            item_to_x -= 0.3
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                character_to_x_LEFT = 0
+        if event.type == pygame.KEYDOWN:  # 키가 눌러졌는지 확인
+            if event.key == pygame.K_LEFT:  # 캐릭터를 왼쪽으로
+                character_to_x -= character_speed
             elif event.key == pygame.K_RIGHT:
-                character_to_x_RIGHT = 0
+                character_to_x += character_speed
             elif event.key == pygame.K_UP:
-                character_to_y_UP = 0
+                character_to_y -= character_speed
             elif event.key == pygame.K_DOWN:
-                character_to_y_DOWN = 0
+                character_to_y += character_speed
 
-    # 게임 캐릭터 위치 정의
-    character_x_pos += character_to_x_LEFT + character_to_x_RIGHT
-    character_y_pos += character_to_y_UP + character_to_y_DOWN
+        if event.type == pygame.KEYUP:  # 방향키를 떼면 멈춤
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_to_x = 0
+            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                character_to_y = 0
 
-    item_x_pos += item_to_x
+    # 3. 게임 캐릭터 위치 정의
+    character_x_pos += character_to_x
+    character_y_pos += character_to_y
 
-    # 가로 경계값 처리
-    if character_x_pos <= 0:
+    if character_x_pos < 0:
         character_x_pos = 0
-    elif character_x_pos >= screen_width - character_width:
-        character_x_pos = screen_width - character_width
-
-    # 세로 경계값 처리
-    if character_y_pos <= 0:
+    elif character_y_pos < 0:
         character_y_pos = 0
-    elif character_y_pos >= screen_height - character_height:
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+    elif character_y_pos > screen_height - character_height:
         character_y_pos = screen_height - character_height
 
+# 3. 게임 캐릭터 위치 정의
+
+    # 4. 충돌 처리
+
+    # 5. 화면에 그리기
+    if random_teacher > teacher_y_pos:
+        teacher_y_pos += 10
+    elif random_teacher < teacher_y_pos:
+        teacher_y_pos -= 10
+    else:
+        if random_teacher == 600:
+            random_teacher = 0
+        else:
+            random_teacher = 600
+
+    ball_time += 2
+    if ball_time == random_time:
+        random_time = random.randrange(100, 200)
+        ball_time = 0
+        ball_y_pos = teacher_y_pos + ball_height/2
+        balls.append([ball_x_pos, ball_y_pos])
+
     screen.blit(background, (0, 0))
-    screen.blit(character, (character_x_pos, character_y_pos))    # 캐릭터 그리기
-    screen.blit(item, (item_x_pos, item_y_pos))      # 적 그리기
+    screen.blit(character, (character_x_pos, character_y_pos))
+    screen.blit(teacher, (teacher_x_pos, teacher_y_pos))
+
+    if len(balls):
+        for ball2 in balls:
+            ball2[0] -= 3
+            screen.blit(ball, (ball2[0], ball2[1]))
+            if ball2[0] <= 0:
+                balls.remove(ball2)
 
     pygame.display.update()
 
+# pygame 종료
 pygame.quit()
